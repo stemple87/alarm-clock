287e8bb91258c8ea4da3a72b5fc74018092f0270
Simon Temple <simonctemple@live.com>
2016-03-18 22:28:17 -0700

    Delete cli.js. unused file.

--- js/cli.js
+++
@@ -1,9 +0,0 @@
-var helloWorld = require('./hello.js').helloWorld;
-var alert = require('alert');
-alert.start();
-
-console.log("Just saying hello!");
-alert.get(helloWorld());
-// result.forEach(function(element) {
-//   console.log(element);
-// });
