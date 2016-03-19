7d1c3b315f1d7dcb0ba4e4211e712522f27b238e
Simon Temple <simonctemple@live.com>
2016-03-18 22:27:30 -0700

    reduce code in time-interface.js

--- js/time-interface.js
+++ js/time-interface.js
@@ -1,19 +1,10 @@
 var getFormattedTime = require('./../js/timeformat.js').getFormattedTime;
-
-// $(document).ready(function(){
-//   var newTime = moment().format("HH:mm");
-//   var theTime = getFormattedTime(newTime);
-//   $('#time').text(theTime);
-// });
-
 var datetime = null,
         date = null;
-
 var update = function () {
     date = moment(new Date());
     datetime.html(date.format('h:mm:ss A'));
 };
-
 $(document).ready(function(){
     datetime = $('#time');
     update();
